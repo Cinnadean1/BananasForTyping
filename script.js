d9d93989147bcf1b3f80c04165fd3ea75d9286cf
Cinnadean1 <cinnadean@outlook.com>
2025-02-05 14:32:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let title = document.getElementById("title");
 let textbox = document.getElementById("textbox");
 
 submitB.onclick = () => {
-  title.innerHTML = textbox;
+  title.innerHTML = textbox.innerHTML;
 }
 
 button.onclick = () => {
