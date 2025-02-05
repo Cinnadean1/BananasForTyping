f4d53651fc3d492982929740ed01346dbb037177
Cinnadean1 <cinnadean@outlook.com>
2025-02-05 14:20:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const button = document.getElementById("button");
+const submitB = document.getElementById("submit");
 const title = document.getElementById("title");
 const textbox = document.getElementById("textbox");
+
+submitB.onclick = () => {
+  title.innerHTML = "test";
+}
 
 button.onclick = () => {
   console.log("the butt")
