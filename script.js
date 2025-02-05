2c05b70a152a9fe97f01a942c50591a2324fad92
Cinnadean1 <cinnadean@outlook.com>
2025-02-05 15:27:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const button = document.getElementById("button");
+
 const submitB = document.getElementById("submit");
 let title = document.getElementById("title");
 let textbox = document.getElementById("textbox");
+let username = "placeholderuser"
 
 submitB.onclick = () => {
   title.innerHTML = textbox.value;
