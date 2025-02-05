db3b0ef25b0b4379688b2e4a187aa8f67d6b22c3
Cinnadean1 <cinnadean@outlook.com>
2025-02-05 14:09:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const button = document.getElementById("button");
+const title = document.getElementById("title");
+const textbox = document.getElementById("textbox");
 
 button.onclick = () => {
   console.log("the butt")
