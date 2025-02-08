8113ec74c576017b91589034a8f1c3e6f40633ff
Cinnadean1 <cinnadean@outlook.com>
2025-02-07 20:59:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const insert = document.getElementById("insert");
+const number = document.getElementById("number");
+
 const button = document.getElementById("bananaB");
 const count = document.getElementById("count");
 let bananas = 0;
@@ -6,6 +9,11 @@ const submitB = document.getElementById("submit");
 let title = document.getElementById("title");
 let textbox = document.getElementById("textbox");
 let username = "placeholderuser"
+
+insert.onclick = () => {
+  bananas = number.value;
+  count.innerHTML = bananas;
+}
 
 submitB.onclick = () => {
   title.innerHTML = textbox.value;
